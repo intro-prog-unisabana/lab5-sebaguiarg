4ea90b780e62ad4e9ae206b5abc1d3dfc9350ddb
Sebastian Aguiar <sebastianaguiar9511@gmail.com>
2026-03-12 14:49:39 -0500

    functions 1.1

--- functions.py
+++ functions.py
@@ -1,10 +1,7 @@
-notas = []
-def promedio(notas):
-    """Definir el nombre de la funcion en base a la entrada en la lista notas"""
-    if len(notas) <= 0:
-        print("Debes incluir las notas")
-    else:
-        return sum(notas)/len(notas)
 
-prom = (promedio(notas))
-print(prom)
+def promedio_estudiante(calificaciones):
+    """Definir el nombre de la funcion en base a la entrada en la lista notas"""
+    if len(calificaciones) <= 0:
+        print("Debes agregar las calificaciones")
+    else:
+        return float(sum(calificaciones)/len(calificaciones))
